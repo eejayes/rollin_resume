47158a48915d46e8018e5f770c3a2d4be2df2879
Erik Stringwell <erik.stringwell@zenseact.com>
2025-04-30 09:26:16 +0200

    Separate configuration from main body
    Style and configuration is a different dimension from the main body in
    principle. Therefore it is separated into a separate file and the build
    command updated to take a configuration parameter.

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+config_file="${1:-config-default}"
+
 mkdir build
-lualatex --output-directory=build --shell-escape resume.tex
+lualatex --output-directory=build --shell-escape "\def\configfile{"$config_file"} \input{resume}"
